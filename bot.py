9c6768f7414db389a1b3520890a0622d0b75b6ac
kasromaha <kasromaha@gmail.com>
2025-08-06 18:48:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ def main() -> None:
     )
 
     scheduler = AsyncIOScheduler(timezone=LOCAL_TZ)
-    scheduler.add_job(send_summary, "cron", hour=18, minute=45)
+    scheduler.add_job(send_summary, "cron", hour=18, minute=55)
     scheduler.start()
 
     application.run_polling()
