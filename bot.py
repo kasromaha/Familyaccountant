f4389679dd32168d7fa3df415fad7fd657821336
kasromaha <kasromaha@gmail.com>
2025-08-06 17:03:46 +0300

    Update bot.py
    Just test

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ def main() -> None:
     )
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_summary, "cron", hour=23, minute=59)
+    scheduler.add_job(send_summary, "cron", hour=14, minute=5)
     scheduler.start()
 
     application.run_polling()
